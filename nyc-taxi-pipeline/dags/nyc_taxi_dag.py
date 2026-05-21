a999a8717e474ce994e128fd7d85337c8fdf542f
LilyL03 <lilyllilyl03@gmail.com>
2026-05-21 22:47:18 +0400

    py's transition to ipynb

--- nyc-taxi-pipeline/dags/nyc_taxi_dag.py
+++ nyc-taxi-pipeline/dags/nyc_taxi_dag.py
@@ -1,65 +1,91 @@
-# dags/nyc_taxi_dag.py
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
+from airflow.contrib.operators.databricks_operator import DatabricksSubmitRunOperator
 
 default_args = {
-    'owner': 'data_engineering',
-    'depends_on_past': True,      
-    'retries': 1,
+    'owner': 'LMe',
+    'depends_on_past': False,
+    'start_date': datetime(2026, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
     'retry_delay': timedelta(minutes=5),
 }
 
-with DAG(
-    dag_id='nyc_taxi_incremental_pipeline',
+dag = DAG(
+    'nyc_taxi_incremental_pipeline',
     default_args=default_args,
     description='Orchestrates NYC Taxi Medallion pipeline on Databricks monthly',
-    schedule_interval='@monthly', 
-    start_date=datetime(2026, 1, 1),
-    catchup=False,                
-    max_active_runs=1,
-) as dag:
+    schedule_interval='@monthly',
+    catchup=False,
+    max_active_runs=1
+)
 
-    cluster_spec = {
-        'spark_version': '14.3.x-scala2.12',
-        'node_type_id': 'Standard_D4s_v5',  
-        'num_workers': 2
-    }
+incremental_params = {
+    "run_date": "{{ ds }}",
+    "environment": "production"
+}
 
-    incremental_params = {
-        "load_type": "incremental",
-        "year": "{{ data_interval_start.strftime('%Y') }}",
-        "month": "{{ data_interval_start.strftime('%m') }}"
-    }
+# For pure serverless workspace - use serverless compute with tasks array
+bronze_ingest = DatabricksSubmitRunOperator(
+    task_id='bronze_ingest',
+    json={
+        'run_name': f'bronze_ingest_{{{{ ds }}}}',
+        'tasks': [
+            {
+                'task_key': 'bronze_ingest_task',
+                'notebook_task': {
+                    'notebook_path': '/Workspace/Users/lilit.levonyan@edu.ysu.am/NYC-Taxi-Data-Engineering-Project/nyc-taxi-pipeline/notebooks/01_bronze_ingest.py',
+                    'base_parameters': incremental_params
+                },
+                'compute': {
+                    'compute_key': 'serverless'  # This tells it to use serverless
+                }
+            }
+        ]
+        # No job_clusters section for pure serverless
+    },
+    dag=dag
+)
 
-    bronze_task = DatabricksSubmitRunOperator(
-        task_id='bronze_ingest',
-        databricks_conn_id='databricks_default',
-        new_cluster=cluster_spec,
-        notebook_task={
-            'notebook_path': '/Repos/production/nyc-taxi-pipeline/notebooks/01_bronze_ingest',
-            'base_parameters': incremental_params
-        }
-    )
+silver_clean = DatabricksSubmitRunOperator(
+    task_id='silver_clean',
+    json={
+        'run_name': f'silver_clean_{{{{ ds }}}}',
+        'tasks': [
+            {
+                'task_key': 'silver_clean_task',
+                'notebook_task': {
+                    'notebook_path': '/Workspace/Users/lilit.levonyan@edu.ysu.am/NYC-Taxi-Data-Engineering-Project/nyc-taxi-pipeline/notebooks/02_silver_clean.py',
+                    'base_parameters': incremental_params
+                },
+                'compute': {
+                    'compute_key': 'serverless'
+                }
+            }
+        ]
+    },
+    dag=dag
+)
 
-    silver_task = DatabricksSubmitRunOperator(
-        task_id='silver_clean',
-        databricks_conn_id='databricks_default',
-        new_cluster=cluster_spec,
-        notebook_task={
-            'notebook_path': '/Repos/production/nyc-taxi-pipeline/notebooks/02_silver_clean',
-            'base_parameters': incremental_params
-        }
-    )
+gold_ml = DatabricksSubmitRunOperator(
+    task_id='gold_ml',
+    json={
+        'run_name': f'gold_ml_{{{{ ds }}}}',
+        'tasks': [
+            {
+                'task_key': 'gold_ml_task',
+                'notebook_task': {
+                    'notebook_path': '/Workspace/Users/lilit.levonyan@edu.ysu.am/NYC-Taxi-Data-Engineering-Project/nyc-taxi-pipeline/notebooks/03_gold_ml.py',
+                    'base_parameters': incremental_params
+                },
+                'compute': {
+                    'compute_key': 'serverless'
+                }
+            }
+        ]
+    },
+    dag=dag
+)
 
-    gold_ml_task = DatabricksSubmitRunOperator(
-        task_id='gold_ml',
-        databricks_conn_id='databricks_default',
-        new_cluster=cluster_spec,
-        notebook_task={
-            'notebook_path': '/Repos/production/nyc-taxi-pipeline/notebooks/03_gold_ml',
-            'base_parameters': incremental_params
-        }
-    )
-
-    bronze_task >> silver_task >> gold_ml_task
+bronze_ingest >> silver_clean >> gold_ml
